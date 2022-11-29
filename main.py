59e76616b95645da2ba1136e91423a2f1ed69b2b
koryto-upjohn <koryto@upjohn.org>
2022-11-29 12:50:28 -0500

    Project setup.

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+
+
+Code Author: Cole Koryto
+"""
+
+# main functions of the program
+def main():
+    print("Hello World")
+
+
+# runs main function if file is called as main file
+if __name__ == '__main__':
+    main()
